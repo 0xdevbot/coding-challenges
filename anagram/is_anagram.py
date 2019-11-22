087aa573de2ea88472f64a139ccf231e62a5bdce
Devin Wallace <devinwallace@protonmail.com>
2019-11-22 13:12:23 -0600

    solution to anagram challenge
    Signed-off-by: Devin Wallace <dwallace2371@gmail.com>

---
+++ anagram/is_anagram.py
@@ -0,0 +1,20 @@
+
+def is_anagram(word1, word2):
+    '''
+    Solution to the anagram problem
+    :param word1: first word
+    :param word2: second word
+    :return: if second word is a possible anagram of word 1 return True else return False
+    '''
+    import re
+    word1_set = set((re.sub("([., '])", "", word1)).lower())
+    print(word1_set)
+    word2_set = set((re.sub("([., '])", "", word2)).lower())
+    print(word2_set)
+    if word1_set == word2_set:
+        return True
+    else:
+        return False
+
+
+print(is_anagram("tE. a", "ea t"))
