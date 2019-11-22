bc24b0939eb8924acd3bbd881b4b1863d21d5c21
Devin Wallace <dwallace2371@gmail.com>
2019-11-22 12:23:25 -0600

    solution to Compact challenge, uses recursion.

--- Compact/Compact.py
+++ Compact/Compact.py
@@ -1,6 +1,14 @@
 def compact(list_in, new_list=[]):
+    '''
+    The purpose of this method is to take a input list of numbers and compact the list down, any recurring numbers
+    that also are agasent will be reduced to a single number
+    EXAMPLE: [1, 1, 2, 2, 3, 2] -> [1,2,3,2]
+    :param list_in: List of numbers to be inputted by the user
+    :param new_list: this is the new list that the method creates
+    :return:  BASE CASE: returns new_list which contains the final output, the output is iter
+    '''
     if len(list_in) == 0:
-        return new_list
+        return iter(new_list)
     else:
         key = list_in[0]
         new_list.append(key)
@@ -11,7 +19,7 @@ def compact(list_in, new_list=[]):
         list_out = list_in[mid:]
         return compact(list_out, new_list)
 
-
-
 print(compact([1, 1, 2, 2, 3, 2]))
+
+
 
